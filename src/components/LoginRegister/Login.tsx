ffd2209bbbbf8ba82b35e759ed0caffcbeb08abf
blincoed <blincoed@reninc.com>
2025-06-19 10:54:30 -0500

    input changes

--- src/components/LoginRegister/Login.tsx
+++ src/components/LoginRegister/Login.tsx
@@ -1,18 +1,20 @@
-import { Input } from "../ui/input";
-import { Label } from "../ui/label";
-import { PasswordInput } from "../ui/password-input";
-
 function Login() {
   return (
     <div className="p-6">
-      <Label htmlFor="email">Email</Label>
-      <div className="mt-2">
-        <Input
-          aria-label="Email"
-          name="email"
+      <div>
+        <label
+          htmlFor="email"
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          id="email"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-400 focus:ring-blue-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          required
         />
       </div>
-      <PasswordInput />
     </div>
   );
 }
--- src/components/LoginRegister/index.tsx
+++ src/components/LoginRegister/index.tsx
@@ -3,7 +3,7 @@ import Register from "./Register";
 
 function LoginRegister() {
   return (
-    <div className="flex h-dvh items-center justify-center bg-orange-100">
+    <div className="flex h-dvh items-center justify-center bg-blue-900">
       <div className="h-full max-h-180 w-full max-w-180 rounded-md bg-white shadow-md">
         <Login />
       </div>
